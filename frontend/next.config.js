025414e7e92a0e56518783096fb5d425fa179da8
Lakshyasaxena0 <lakshyasaxena0@gmail.com>
2026-04-13 18:46:33 +0530

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,15 @@
+// frontend/next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode:  true,
+  output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
+};
+module.exports = nextConfig;
